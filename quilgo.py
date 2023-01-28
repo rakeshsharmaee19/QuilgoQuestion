df00c68ec9c9ee18b05630d7d04bab6a7702010d
rakeshsharmaee19 <91816394+rakeshsharmaee19@users.noreply.github.com>
2023-01-28 16:22:03 +0530

    Add files via upload

---
+++ quilgo.py
@@ -0,0 +1,19 @@
+def longestCommanSubString(x,y,m,n):
+    longestComman = [[0 for k in range(n+1)] for l in range(m+1)]
+    result =0
+    for i in range(m+1):
+        for j in range(n+1):
+            if i==0 or j==0:
+                longestComman[i][j]==0
+            elif x[i-1] == y[j-1]:
+                longestComman[i][j] = longestComman[i-1][j-1]+1
+                result = max(result, longestComman[i][j])
+            else:
+                longestComman[i][j] = 0
+    return result
+            
+x = input('enter the first string : ')
+y = input('enter the second string : ')
+m = len(x)
+n = len(y)
+print('Length of longest comman substring is ',longestCommanSubString(x,y,m,n))
